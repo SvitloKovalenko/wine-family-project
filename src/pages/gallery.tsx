8319b285e6445d64bd9726fcb77f6b5bf8c3ee9e
Svitlana Kovalenko <kovalenkosv60@gmail.com>
2023-09-16 10:31:04 +0300

    single page

--- src/pages/gallery.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "@/components/layout/Layout";
-import Gallery from "@/components/screens/gallery/Gallery";
-import { NextPage } from "next";
-
-const GalleryPage: NextPage = () => {
-  return <Gallery />;
-};
-
-export default GalleryPage;
